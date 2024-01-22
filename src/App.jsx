783877349805758d73687cc21bfb8ffd151370f1
rubenskuhn <rubenskuhn@gmail.com>
2024-01-22 16:26:31 +0100

    pop da cherry

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Form from "./components/Form.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Form />
+    </>
+  );
+}
+
+export default App;
+
+/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +35,4 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> */
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+export default function Form() {
+  return (
+    <form>
+      <h1>Add New Activity</h1>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+    </form>
+  );
+}
